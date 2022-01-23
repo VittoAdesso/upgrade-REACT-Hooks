971ba8afd1a27c8acfc352960c8c23cef3fd4b32
Vittorio Adesso <adessovittorio8@gmail.com>
2022-01-23 20:38:48 +0100

    Modf

--- hooks2/src/App.jsx
+++ hooks2/src/App.jsx
@@ -1,9 +1,17 @@
 
 import './App.css';
+import { UseEffect } from './components/UseEffect/UseEffect';
+
+import UseEffectInput from './components/UseEffectInput/UseEffectInput';
+
 
 function App() {
   return (
     <div className="App">
+      
+      <UseEffect/>
+ 
+      <UseEffectInput />      
       
     </div>
   );
